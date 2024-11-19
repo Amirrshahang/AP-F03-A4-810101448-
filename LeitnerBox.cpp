b49a576ad1628fd412ffe351838538eecc3c19d1
Amir's macbook <amir@Amirs-MacBook-2.local>
2024-11-19 17:24:26 +0330

    Add functionality to add flashcards to the daily box and update to_string method

--- LeitnerBox.cpp
+++ LeitnerBox.cpp
@@ -34,23 +34,37 @@ private:
 
 class LeitnerBox{
 public:
-	string to_string(){
+    string to_string(){
         ostringstream os;
         for ( auto& card : cards) {
             os << card.to_string() << endl;
         }
         return os.str();
     }
+    void add_flashcards(int number_of_questions) {
+        string question, answer;
+        for (int i = 1; i <= number_of_questions; ++i) {
+            getline(cin, question);
+            getline(cin, answer);
+            cards.emplace_back(question, answer);
+        }
+        cout << "Flashcards added to the daily box" << endl;
+    }
+    
 private:
     vector<Card> cards;
 };
 int main() {
     LeitnerBox daily_box;
 
-	string command;
+    string command;
     while (getline(cin, command)) {
-       
-		 if (command == "dump") {
+        if (command.find("add_flashcard")==0) {
+            istringstream iss(command.substr(14));
+            int number_of_questions;
+            iss >> number_of_questions;
+            daily_box.add_flashcards(number_of_questions);
+        }else if (command == "dump") {
             cout << daily_box.to_string() << endl;
         }
     }
